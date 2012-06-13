6adb8f87d4669f594a1092540fc8d877f2e2285a
Jonathan Gottfried <jonmarkgo@gmail.com>
2012-06-12 20:27:32 -0400

    Got my bot to talk to my node server

--- server.js
+++ server.js
@@ -1,41 +1,13 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs')
-  , net = require('net')
+var express = require('express'),
+    app = express.createServer(),
+    net = require('net');
+var twilioAPI = require('twilio-api'),
+    cli = new twilioAPI.Client(process.env.account_sid, process.env.auth_token);
+app.use(cli.middleware() );
+app.listen(8002);
 
-arduinoTcp = null;
-
-app.listen(8090);
-
-function handler (req, res) {
-  fs.readFile(__dirname + '/arduino.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading arduino.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.sockets.on('connection', function (socket) {
-  socket.on('gobuttonclick', function (data) {
-if (arduinoTcp != null) {
-        arduinoTcp.write(data.direction.toString());
-       console.log("Sending direction " + data.direction);
-    }
-  });
-  socket.on('sonarbuttonclick', function (data) {
-if (arduinoTcp != null) {
-        arduinoTcp.write('5');
-        console.log("Sending 5");
-    }
-  });
-});
-
-//tcp socekt server
+var arduinoTcp = null;
+var curr_call = null;
 var tcpServer = net.createServer(function (socket) {
   console.log('tcp server running on port 1337');
 });
@@ -49,11 +21,47 @@ tcpServer.on('connection',function(socket){
     socket.on('data',function(mydata){
 
         console.log('received on tcp socket:'+mydata);
-
+       curr_call.load(function(err,call) {
+       	if (mydata != '2OK' && mydata != '8OK') {
+       		curr_call = call;
+       		 call.liveCb(function(err,subcall) {
+       		 	subcall.gather(getDigits, {numDigits: 1}).say(mydata + " inches");
+       		 });
+       	}
+        });
         //socket.write('msg received\r\n');
-            io.sockets.emit('tcpreply',{reply:mydata + ''});
+        //    io.sockets.emit('tcpreply',{reply:mydata + ''});
         }
     );
 });
 tcpServer.listen(1337);
 
+
+//Get a Twilio application and register it
+function getDigits(call,input) {
+	   console.log('pressed ' + input);
+      arduinoTcp.write(input);
+      curr_call = call;
+    call.gather(getDigits, {numDigits: 1});
+
+  /*twilio_app.listCalls({}, function(err,li) {
+console.log(li);
+   });*/
+      }
+   
+      function saySonar(err,call) {
+      	call.say('10 inches');
+      }
+var twilio_app = cli.account.getApplication(process.env.app_sid, function(err, app) {
+    if(err) throw err;
+    app.register();
+    app.on('incomingCall', function(call) {
+    	if (arduinoTcp == null) {
+    		call.say('No bot connected.');
+    	}
+    	curr_call = call;
+       call.gather(getDigits, {numDigits: 1}).say("Beep boop beep");
+     
+    });
+
+});
